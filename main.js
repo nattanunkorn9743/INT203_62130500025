7989407990ad526ed72ff2e0ae24461c87c6a29f
nattanunkorn <nattanunkorn.cake@mail.kmutt.ac.th>
2021-01-28 17:38:36 +0700

    [navbar] chang t-color

--- main.js
+++ main.js
@@ -7,3 +7,14 @@
 
     }
     Vue.createApp(app).mount('#app')
+
+    module.exports = {
+        theme: {
+         textColor: theme => theme('colors'),
+         textColor: {
+           'primary': '#3490dc',
+           'secondary': '#ffed4a',
+           'danger': '#e3342f',
+         }
+        }
+      }
